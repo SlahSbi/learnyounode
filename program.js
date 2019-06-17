88dd27524d45488395565ea839dc07b48cf6b056
slah sbi <slahsbi@gmail.com>
2019-06-17 10:15:51 +0100

    first commit

---
+++ program.js
@@ -0,0 +1,34 @@
+//select 0
+//console.log('HELLO WORLD')
+
+//-------------------------------------
+//select 1
+/*sum=(tab)=>{
+    return s=tab.map((el)=>(Number(el)?Number(el):0)).reduce((a,b)=>(a+b))
+ }
+ console.log(sum(process.argv))
+*/
+
+//-------------------------------------
+//select 2
+//console.log(require('fs').readFileSync(process.argv[2]).toString().split('\n').length-1)
+
+//-------------------------------------
+//select 3
+//require('fs').readFile(process.argv[2], 'utf8', (err, data) => {console.log((data.toString().split('\n').length-1))})
+
+//-------------------------------------
+//select 4
+
+//  let s = require('fs').readdir(process.argv[2], (err, data) => {
+//    data.filter((el) => (new RegExp(`[a-zA-Z0-9_-]{1,}.(${process.argv[3]})`).test(el))).forEach(el=> {
+//     console.log(el)
+//   }) })
+
+
+
+
+
+
+
+
